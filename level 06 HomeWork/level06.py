943c02d0a5f94f6a9d34d5a48bc58729a383dcb3
GEORGEqubi <gioqubi@gmail.com>
2025-10-19 02:06:14 +0400

    level06

---
+++ level 06 HomeWork/level06.py
@@ -0,0 +1,26 @@
+#  კომენტარებით ახსენით რა არის input/output-ი და მოიყვანეთ მათი მაგალითები.
+
+#  input-არის პროცესი როდესაც კომპიუტერში შედის ინფორმაცია. მაგალითად-კლავიატურაზე წერისას, მიკროფონში ლაპარაკისას.
+#  output-არის პროცესი როდესაც კომპიუტერიდან ვიღებთ რაიმე ინორმაციას. მაგ-მონიტორზე გამოსული გამოსახულება, დინამიკებიდან გამოსული ხმა.
+
+
+# მომხმარებელს შემოატანინეთ ორი სიტყვა, შეინახეთ ისინი ცვლადში, მოახდინეთ მათი კონკადინაცია და დაბეჭდეთ
+
+
+name = input("What is your name?: ")
+surname = input("What is your lastname: ")
+
+print("hello " + name + surname)
+
+# მომხმარებელს შემოატანინეთ 5 რიცხვი და დაბეჭდეთ მათი საშუალო არითმეტიკული(დაგჭირდებათ int ფუნქცია).
+
+num1 =int(input("choose any number one to ten: "))
+num2 =int(input("choose any number one to ten: "))
+num3 =int(input("choose any number one to ten: "))
+num4 =int(input("choose any number one to ten: "))
+num5 =int(input("choose any number one to ten: "))
+
+
+print (int((num1 + num2 + num3 + num4 +num5) / 2))
+
+
