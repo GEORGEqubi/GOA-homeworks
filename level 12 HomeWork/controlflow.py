fd055811c7776106ed5c721d731462f04e3efe0e
GEORGEqubi <gioqubi@gmail.com>
2025-11-20 19:54:38 +0400

    level12

---
+++ level 12 HomeWork/controlflow.py
@@ -0,0 +1,53 @@
+
+# sequencing - კომპიუტერის მიერი წაკითხული კოდი თანმიმდევრობით დასაწყისიდან ბოლომდე.
+
+# iteration - ერთიდაიგივეს გამოერება ციკლი.
+
+# selection -  კომპიუტერი იღებს გადაწყვეტილებას კითხვაზე დაყრდნობით.
+
+
+# ალგორითმი - სწორი თანმიმდევრობით გაწერილი ინსტრუქცია დავალების შესასრულებლად. მაგ-ნებისმიერი კერძის რეცეპტი
+
+# ალგორითმს ავღწერთ სამი სახით - flowchart, pseudocode, natural language.
+
+my_name = "George"
+
+user_input = input("Enter the name: ")
+
+print(my_name==user_input)
+
+'''
+natural language
+
+პროგრამა ქმნის ცვლადს my_name და ანიჭებს მნიშვნელობას "George".
+
+ შემდეგ პროგრამა ეკითხება მომხმარებელს სახელს და ინახავს შეყვანილ ტექსტს ცვლადში user_input.
+
+პროგრამა ადარებს: my_name == user_input
+
+ შედარების შედეგი (True ან False) იბეჭდება ეკრანზე.
+
+ pseudecode
+
+ START
+
+ SET my_name TO "George"
+
+ GET user_input FROM USER
+
+ IF user_input EQUALS my_name THEN
+     PRINT True
+ ELSE
+    PRINT False
+ END IF    
+
+ END
+
+ '''
+
+  
+
+
+
+
+
