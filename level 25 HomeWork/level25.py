50ab452f0685981c49e905cc87ac824844414947
GEORGEqubi <gioqubi@gmail.com>
2026-01-17 22:12:00 -0600

    level25

---
+++ level 25 HomeWork/level25.py
@@ -0,0 +1,20 @@
+# index-ი არის ელემენტის მდებარეობის ნომერი მონაცემთა სტრუქტურაში
+# მაგალითად: სიაში (list), სტრიქონში (string)
+
+text = "Python"
+
+print(text[0])  # გამოიტანს: P
+print(text[3])  # გამოიტანს: h
+
+
+print(text[-1])  # გამოიტანს: n 
+print(text[-2])  # გამოიტანს: o
+print(text[-6])  # გამოიტანს: P 
+
+numbers = [10, 20, 30, 40, 50]
+
+print(numbers[0])  # გამოიტანს: 10
+print(numbers[2])  # გამოიტანს: 30
+
+print(numbers[-1])  # გამოიტანს: 50
+print(numbers[-2])  # გამოიტანს: 40
