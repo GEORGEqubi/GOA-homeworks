f5c4e134f7ed6fbd3f2f824682219fe81e9c2d09
GEORGEqubi <gioqubi@gmail.com>
2026-06-02 17:33:49 -0500

    level 65

---
+++ level 65 HomeWork/script.js
@@ -0,0 +1,88 @@
+// მიმატება (+)
+console.log(5 + 3); 
+
+// გამოკლება (-)
+console.log(10 - 4); 
+
+// გამრავლება (*)
+console.log(6 * 2); 
+
+// გაყოფა (/)
+console.log(20 / 5); 
+
+// ნაშთის პოვნა (%)
+console.log(10 % 3); 
+
+// ხარისხში აყვანა (**)
+console.log(2 ** 3); 
+
+// ინკრემენტი (++)
+let a = 5;
+a++;
+console.log(a);
+
+// დეკრემენტი 
+let b = 5;
+b--;
+console.log(b); 
+
+
+
+
+
+
+
+// Number
+let age = 16;
+console.log(typeof age); 
+
+// String
+let name = "Giorgi";
+console.log(typeof name); 
+
+// Boolean
+let isStudent = true;
+console.log(typeof isStudent);
+
+// Undefined
+let x;
+console.log(typeof x); 
+
+
+
+
+let num = 10;
+
+// მიმატება
+num += 5;
+console.log(num); 
+
+// გამოკლება
+num -= 3;
+console.log(num); 
+
+// გამრავლება
+num *= 2;
+console.log(num); 
+
+// გაყოფა
+num /= 4;
+console.log(num); 
+
+// ნაშთის პოვნა
+num %= 4;
+console.log(num); 
+
+// ხარისხში აყვანა
+num **= 3;
+console.log(num); 
+
+
+let firstName = "Giorgi";
+let lastName = "Kubiashvili";
+
+console.log(firstName + " " + lastName);
+
+let age2 = 18;
+
+console.log("I am " + age + " years old.");
